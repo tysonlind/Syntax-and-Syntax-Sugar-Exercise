8dc5c84adef25549c288bb0bbbec0945faaed51b
tyson lind <tysonfloydlind@gmail.com>
2023-04-13 09:09:33 -0600

    completed exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
         }
     }
 }
